47f245c16e70c501664273171b532b319724e881
sandya0 <66195253+sandya0@users.noreply.github.com>
2023-10-14 22:31:46 +0700

    navbar

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,18 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restoran</title>
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+    </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="mt-5 justify-content-center">
-            <a href="loginAndRegister/login.php" class="btn btn-primary me-2">Login</a>
-            <a href="loginAndRegister/register.php" class="btn btn-secondary me-2">Register</a>
-            <a href="bridge/bridge.php" class="btn btn-danger">Account</a>
+
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#top">Restoran</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="loginAndRegister/login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="loginAndRegister/register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    </nav>
+
 
 </body>
+
 
 </html>
