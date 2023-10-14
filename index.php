ad69ec9282e44e90e47863f2e561db751d73e5e0
sandya0 <66195253+sandya0@users.noreply.github.com>
2023-10-14 22:58:56 +0700

    namba file (blom fix)

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restoran</title>
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
     </style>
@@ -30,6 +31,10 @@
         </div>
     </nav>
 
+    <div class="title-container">
+        <h1>Restaurant Name</h1>
+        <p>Your Slogan Here</p>
+    </div>
 
 </body>
 
