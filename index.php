2cf5aec3f3c5ea0d264664be4db1621f1edcf5b5
sandya0 <66195253+sandya0@users.noreply.github.com>
2023-10-21 21:23:09 +0700

    design lah pokonya

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include('database/connect.php');
-var_dump($_SESSION['user_id']);
 ?>
 
 <!DOCTYPE html>
@@ -11,20 +10,17 @@ var_dump($_SESSION['user_id']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restoran</title>
-    <!-- <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="css/index.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-    </style>
 </head>
 
 <body>
-
     <!-- Navigation bar -->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#index.php">
+            <a class="navbar-brand" href="#">
                 Restoran
             </a>
             <div class="collapse navbar-collapse">
@@ -42,23 +38,49 @@ var_dump($_SESSION['user_id']);
             </div>
         </div>
     </nav>
-    <!-- Mau eksperimen dulu -->
-    <!--
+
     <div class="title-container" data-aos="fade-up" data-aos-duration="1000">
         <h1 data-aos="fade-up" data-aos-duration="1000">Restaurant Name</h1>
         <p data-aos="fade-up" data-aos-duration="1000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque suscipit tenetur sed itaque minima officiis quis amet quam provident! Sunt, <br> laudantium quae. Voluptates facilis harum non ad quaerat modi veniam?</p>
         <div class="button-container">
-          <button class="btn">
-            Get Started
-          </button>
+            <button class="btn scrollto" href="#about">
+                Get Started
+            </button>
+        </div>
     </div>
-    -->
+
+    <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+            <div class="row">
+                <div class="col-lg-6 order-1 o  rder-lg-2" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="about-img">
+                        <img src="css/gambar/about.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+                    <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+                    <p class="fst-italic">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <ul>
+                        <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                        <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                        <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                    </ul>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste odit voluptatum voluptatibus iure! Temporibus perspiciatis deleniti veniam rerum porro impedit hic dolores a asperiores consectetur et eveniet, illum omnis sequi.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
     <?php
     if (isset($_POST['add_to_cart'])) {
         $id_produk = $_POST['id_produk'];
         $id_user = $_POST['id_user'];
 
-        // Periksa apakah produk sudah ada di keranjang
         $check_query = "SELECT * FROM cart_table WHERE product_id = '$id_produk' AND user_id = '$id_user'";
         $check_result = mysqli_query($conn, $check_query);
 
@@ -87,35 +109,139 @@ var_dump($_SESSION['user_id']);
 
     ?>
 
-    <div class="container col-8 mt-5">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <?php
-            $sql = "SELECT * FROM data_makanan";
-            $result = mysqli_query($conn, $sql);
-            foreach ($result as $key) : ?>
-                <div class="col">
-                    <div class="card h-100">
-                        <!-- tolong ini biar bentuknya menyesuaikan card, bisa dipencet yang akan muncul deskripsi, bisa di close -->
-                        <div class="img-container">
-                            <img src="admin/uploads/<?= $key['gambar_menu'] ?>" class="card-img-top" alt="<?= $key['gambar_menu'] ?>">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $key['nama_menu'] ?></h5>
-                        </div>
-                        <form action="" method="post">
-                            <button class="btn btn-primary" type="submit" name="add_to_cart">Tambah</button>
-                            <input type="hidden" name="id_produk" value="<?= $key['id_menu'] ?>">
-                            <input type="hidden" name="id_user" value="<?= $_SESSION['user_id'] ?>">
-                        </form>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+    <section id="menu" class="menu section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Menu</h2>
+          <p>Check Our Tasty Menu</p>
         </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-lg-12 d-flex justify-content-center">
+        <ul id="menu-flters">
+            <li data-filter="*" class="filter-active">All</li>
+            <li data-filter=".filter-Appetizer">Appetizer</li>
+            <li data-filter=".filter-MainCourse">Main Course</li>
+            <li data-filter=".filter-SideDish">Side Dish</li>
+            <li data-filter=".filter-Beverages">Beverages</li>
+            <li data-filter=".filter-Dessert">Dessert</li>
+        </ul>
     </div>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+</div>
+
+<div class="row menu-container" data-aos="fade-up" data-aos-delay="200">
+
+    <?php
+    $sql = "SELECT * FROM data_makanan";
+    $result = mysqli_query($conn, $sql);
+    foreach ($result as $key) : ?>
+        <div class="col-lg-6 menu-item filter-<?= str_replace(' ', '', $key['kategori_menu']) ?>">
+            <!-- tolong ini biar bentuknya menyesuaikan card, bisa dipencet yang akan muncul deskripsi, bisa di close -->
+            <div class="img-container">
+                <img src="admin/uploads/<?= $key['gambar_menu'] ?>" class="menu-img" alt="<?= $key['gambar_menu'] ?>">
+            </div>
+            <div class="menu-content">
+                <a class="menu-title"><?= $key['nama_menu'] ?></a>
+                <span>Rp.<?= $key['harga_menu'] ?></span>
+            </div>
+            <form action="" method="post">
+                <div class="button-container">
+                <button class="add btn" type="submit" name="add_to_cart">Tambah</button>
+                <input type="hidden" name="id_produk" value="<?= $key['id_menu'] ?>">
+                <!-- <input type="hidden" name="id_user" value="<?= $_SESSION['user_id'] ?>"> -->
+                </div>
+            </form>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+
+
+
+
+      </div>
+    </section><!-- End Menu Section -->
+
+
+
+
+      <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>Restoran</h3>
+              <p>
+
+              </p>
+
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Our Email</h4>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span></span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/restaurantly-restaurant-template/ -->
+        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+      </div>
+    </div>
+    
+  </footer><!-- End Footer -->
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
     <script>
         AOS.init();
-    </script> -->
+    </script>
+    <script>
+    $(document).ready(function () {
+        // Initialize Isotope
+        $('.menu-container').isotope({
+            itemSelector: '.menu-item',
+            layoutMode: 'fitRows'
+        });
 
+        // Filter items on button click
+        $('#menu-flters li').on('click', function () {
+            $('#menu-flters li').removeClass('filter-active');
+            $(this).addClass('filter-active');
+            var selector = $(this).data('filter');
+            $('.menu-container').isotope({ filter: selector });
+        });
+    });
+</script>
 
 </html>
