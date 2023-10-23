200b477a1339f7e192fa24ed1199f687327e2537
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2023-10-23 22:09:31 +0700

    salah astaga

--- loginAndRegister/register.php
+++ loginAndRegister/register.php
@@ -20,14 +20,14 @@ if (isset($_SESSION['user_login'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
         <a class="navbar-brand" href="../index.php">Cafetarian</a>
     </div>
 </nav>
 
 <body>
-    <div class="container col-4">
+    <div class="container col-4 mt-3">
         <div class="card">
             <div class="card-body">
                 <?php
