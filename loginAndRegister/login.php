da874fd562622e772ccbf318002008e64b2176e3
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2023-10-24 14:52:34 +0700

    login

--- loginAndRegister/login.php
+++ loginAndRegister/login.php
@@ -9,106 +9,112 @@ if (isset($_SESSION['user_login'])) {
         // exit;
     }
 }
+
+if (isset($_POST['submit_login'])) {
+    require "captcha.php"; // Pastikan file captcha.php ada
+    $PHPCAP = new Captcha(); // Inisialisasi objek Captcha
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    require_once('../database/connect.php'); // Pastikan file connect.php ada
+
+    // SQL Injection
+    $sql = "SELECT * FROM access_table WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user_login = $result->fetch_assoc();
+    $user_id = $user_login['id'];
+
+    if (!$user_login) {
+        echo "<div class='alert alert-danger'>Username tidak ditemukan</div>";
+    } else {
+        // Verifikasi password
+        if (password_verify($password, $user_login["password"])) {
+            // Verifikasi captcha
+            $captchaInput = $_POST['captcha'];
+            if (!$PHPCAP->verify($captchaInput)) {
+                echo "<div class='alert alert-danger'>Captcha tidak sesuai</div>";
+            } else {
+                // Captcha benar, set session dan arahkan ke halaman berikutnya
+                $_SESSION['user_login'] = true;
+                if ($user_login['role'] == 0) {
+                    $_SESSION['role'] = 0;
+                    $_SESSION['user_id'] = $user_id;
+                    header('location: ../bridge/bridge.php');
+                    exit;
+                } else {
+                    $_SESSION['role'] = 1;
+                    $_SESSION['user_id'] = $user_id;
+                    header('location: ../bridge/bridge.php');
+                    exit;
+                }
+            }
+        } else {
+            echo "<div class='alert alert-danger'>Password salah</div>";
+        }
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register Page</title>
+    <title>Login Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body {
+            padding-top: 80px;
+        }
+    </style>
 </head>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
     <div class="container">
         <a class="navbar-brand" href="../index.php">Cafetarian</a>
     </div>
 </nav>
 
 <body>
-    <div class="container col-4 mt-3">
+    <div class="container col-4">
         <div class="card">
             <div class="card-body">
-                <?php
-                // cek kalo semua data sudah masuk atau belum
-                if (isset($_POST['submit_register'])) {
-                    $first_name = $_POST['first_name'];
-                    $last_name = $_POST['last_name'];
-                    $birth_date = $_POST['birth_date'];
-                    $gender_type = $_POST['gender_type'];
-                    $username = $_POST['username'];
-                    $passwordBefore = $_POST['password'];
-                    $password = password_hash($passwordBefore, PASSWORD_DEFAULT);
-
-                    // alert bakalan keluar kalo ada yang belum diisi atau tidak sesuai dengan format
-                    $error_message = array();
-                    if (empty($first_name) or empty($last_name) or empty($birth_date) or empty($gender_type) or empty($username) or empty($passwordBefore)) {
-                        array_push($error_message, "Semua data harus diisi");
-                    }
-
-                    require_once('../database/connect.php');
-                    // cek apakah username sudah ada atau belum
-                    $sql = "SELECT * FROM access_table WHERE username = '$username'";
-                    $result = mysqli_query($conn, $sql);
-                    $rowCount = mysqli_num_rows($result);
-                    if ($rowCount > 0) {
-                        array_push($error_message, "Username sudah ada");
-                    }
-
-                    // munculin alert
-                    if (count($error_message) > 0) {
-                        // ini kalo ada yang error
-                        foreach ($error_message as $error) {
-                            echo "<div class='alert alert-danger'>$error</div>";
-                        }
-                    } else {
-                        $sql = "INSERT INTO access_table (first_name, last_name, gender_type, birth_date, username, password, role)
-                VALUES ('$first_name', '$last_name', '$gender_type', '$birth_date', '$username', '$password', 1)";
-                        $result = mysqli_query($conn, $sql);
-                        if ($result) {
-                            echo "<div class='alert alert-success'>Register berhasil</div>";
-                        } else {
-                            echo "<div class='alert alert-danger'>Register gagal</div>";
-                        }
-                    }
-                }
-                ?>
-                <h2 class="fw-bold">Register</h2>
+                <h2 class="fw-bold">Log in</h2>
                 <form action="" method="post">
-                    <div class="form-group">
-                        <label for="first_name">Nama Depan</label>
-                        <input type="text" class="form-control" name="first_name">
-                    </div>
-                    <div class="form-group">
-                        <label for="last_name">Nama Belakang</label>
-                        <input type="text" class="form-control" name="last_name">
-                    </div>
-                    <div class="form-group">
-                        <label for="gender_type">Gender</label>
-                        <select name="gender_type" class="form-control">
-                            <option value="m">Male</option>
-                            <option value="f">Female</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="birth_date">Tanggal Lahir</label>
-                        <input type="date" class="form-control" name="birth_date">
-                    </div>
-                    <div class="form-group">
+                    <div class="form-outline">
                         <label for="username">Username</label>
                         <input type="text" class="form-control" name="username">
                     </div>
-                    <div class="form-group">
+                    <div class="form-outline">
                         <label for="password">Password</label>
                         <input type="password" class="form-control" name="password">
                     </div>
+                    <div class="form-group">
+                        <label class="form-label">Confirm that you're a human</label>
+                    </div>
+                    <div class="form-group">
+                        <?php
+                        include('./captcha.php');
+                        $PHPCAP->prime();
+                        $PHPCAP->draw();
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Complete the Captcha</label>
+                    </div>
+                    <div class="form-group col-4">
+                        <input type="text" name="captcha" class="form-control" required>
+                    </div>
                     <div class="form-btn mt-2">
-                        <input type="submit" class="btn btn-primary" value="Register" name="submit_register">
+                        <input type="submit" class="btn btn-primary" value="Login" name="submit_login">
                     </div>
                 </form>
                 <div class="mt-2">
-                    <p>Already registered? <a href="login.php">Login</a></p>
+                    <p>Not registered yet? <a href="register.php">Register</a></p>
                     <p>Back to homepage <a href="../index.php">Homepage</a></p>
                 </div>
             </div>
